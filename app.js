eab744c645859b1cfff22969b3ab6715695751ad
James Gibson Weber <j.gibweb@gmail.com>
2015-01-06 13:17:38 -0800

    initial commit

--- app.js
+++ app.js
@@ -1,33 +1,52 @@
 var express = require('express');
 var app = express();
-var mongoose       = require("mongoose");
+var bodyParser = require('body-parser');
+var mongoose = require("mongoose");
+var path = require('path');
 
-// app.use(express.static('public'));
-var logger = require('./logger');
-app.use(logger);
+app.use(express.static(path.join(__dirname, 'public')));
+app.use('/bower_components',  express.static(path.join(__dirname, 'bower_components')));
+app.use(bodyParser.urlencoded({extended:false}));
 
-app.get('/', function(req, res) {
-  res.sendFile(__dirname + "/public/index.html");
+
+
+var Linkedin = require('node-linkedin')(process.env.LINKEDIN_API, process.env.LINKEDIN_SECRET, 'localhost:3000/oauth/linkedin/loggedin');
+var linkedin;
+var access_token;
+linkedin = Linkedin.init(access_token, { timeout: 10000 });
+
+app.get('/oauth/linkedin', function(req, res) {
+    Linkedin.auth.authorize(res, ['r_basicprofile', 'r_fullprofile', 'r_emailaddress', 'r_network', 'r_contactinfo', 'rw_nus', 'rw_groups', 'w_messages']);
 });
 
-var blockRoutes = require('./routes/blocks');
-app.use('/blocks', blockRoutes);
+app.get('/oauth/linkedin/loggedin', function(req, res) {
+    Linkedin.auth.getAccessToken(res, req.query.code, function(err, results) {
+        if (err) {return console.error(err);}
 
-var todoRoutes = require('./routes/todo');
-app.use('/todo', todoRoutes);
+        access_token = JSON.parse(results).access_token;
+        linkedin = Linkedin.init(access_token, {
+          timeout: 10000 
+        });
+        
+        return res.redirect('/');
+    });
+});
 
-
-
-// MongoDB config
-mongoose.connect('mongodb://jweber000:M4cb00kpr0!@ds053140.mongolab.com:53140/sandbox', function(err, res) {
-  if(err) {
-    console.log('error connecting to MongoDB Database. ' + err);
-  } else {
-    console.log('Connected to Database');
-  }
+app.get('/:name', function(req, res) {
+  
+  linkedin.companies.name(req.params.name, function(err, company) {
+      if (err) {
+        res.send(err);  
+      } else {
+        res.send(company);
+      }
+  });
+  
 });
 
 
-app.listen(3000, function() {
-  console.log('shh, im working over here...')
+
+var port = process.env.PORT || 3000;
+app.listen(port, function() {
+  console.log('Server is working')
 });
